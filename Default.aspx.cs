7a7b39011bbdd85c1606a4d6f48a2c6f1c41b1e6
S <selvan.s@capgemini.com>
2017-04-24 15:51:52 +0530

    Signed-off-by: S <selvan.s@capgemini.com>

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -13,5 +13,10 @@ namespace Sample2
         {
 
         }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Home.aspx");
+        }
     }
 }
